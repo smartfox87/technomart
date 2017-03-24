64aa85774eab3ce3e532b8e59789452be767c2b7
Andrei Dyminski <smoke59@mail.ru>
2017-03-24 18:13:59 +0300

    init project

---
+++ block.js
@@ -0,0 +1,157 @@
+'use strict';
+
+// Генератор файлов блока
+
+// Использование: node block.js [имя блока] [доп. расширения через пробел]
+
+const fs = require('fs');                // будем работать с файловой системой
+const pjson = require('./package.json'); // получим настройки из package.json
+const dirs = pjson.config.directories;   // отдельно имеем объект с директориями (где лежаи папка с блоками)
+const mkdirp = require('mkdirp');        // зависимость
+
+let blockName = process.argv[2];          // получим имя блока
+let defaultExtensions = ['scss', 'html']; // расширения по умолчанию
+let extensions = uniqueArray(defaultExtensions.concat(process.argv.slice(3)));  // добавим введенные при вызове расширения (если есть)
+
+// Если есть имя блока
+if(blockName) {
+
+    let dirPath = dirs.source + '/blocks/' + blockName + '/'; // полный путь к создаваемой папке блока
+    mkdirp(dirPath, function(err){                            // создаем
+
+        // Если какая-то ошибка — покажем
+        if(err) {
+            console.error('[NTH] Отмена операции: ' + err);
+        }
+
+        // Нет ошибки, поехали!
+        else {
+            console.log('[NTH] Создание папки ' + dirPath + ' (создана, если ещё не существует)');
+
+            // Читаем файл диспетчера подключений
+            let connectManager = fs.readFileSync(dirs.source + '/scss/style.scss', 'utf8');
+
+            // Делаем из строк массив, фильтруем массив, оставляя только строки с незакомментированными импортами
+            let fileSystem = connectManager.split('\n').filter(function(item) {
+                if(/^(\s*)@import/.test(item)) return true;
+                else return false;
+            });
+
+            // Обходим массив расширений и создаем файлы, если они еще не созданы
+            extensions.forEach(function(extention){
+
+                let filePath = dirPath + blockName + '.' + extention; // полный путь к создаваемому файлу
+                let fileContent = '';                                 // будущий контент файла
+                let SCSSfileImport = '';                              // конструкция импорта будущего SCSS
+                let fileCreateMsg = '';                               // будущее сообщение в консоли при создании файла
+
+                // Если это SCSS
+                if(extention == 'scss') {
+                    SCSSfileImport = '@import \'' + dirs.source + '/blocks/' + blockName + '/' + blockName + '.scss\';';
+                    fileContent = '// Для импорта в диспетчер подключений: ' + SCSSfileImport + '\n\n@import \'../../scss/variables.scss\';     // только для удобства обращения к переменным\n\n\n.' + blockName + ' {\n  \n}\n';
+                    // fileCreateMsg = '[NTH] Для импорта стилей: ' + SCSSfileImport;
+
+                    // Создаем регулярку с импортом
+                    let reg = new RegExp(SCSSfileImport, '');
+
+                    // Создадим флаг отсутствия блока среди импортов
+                    let impotrtExist = false;
+
+                    // Обойдём массив и проверим наличие импорта
+                    for (var i = 0, j=fileSystem.length; i < j; i++) {
+                        if(reg.test(fileSystem[i])) {
+                            impotrtExist = true;
+                            break;
+                        }
+                    }
+
+                    // Если флаг наличия импорта по-прежнему опущен, допишем импорт
+                    if(!impotrtExist) {
+                        // Открываем файл
+                        fs.open(dirs.source + '/scss/style.scss', 'a', function(err, fileHandle) {
+                            // Если ошибок открытия нет...
+                            if (!err) {
+                                // Запишем в конец файла
+                                fs.write(fileHandle, SCSSfileImport + '\n', null, 'utf8', function(err, written) {
+                                    if (!err) {
+                                        console.log('[NTH] В диспетчер подключений ('+ dirs.source + '/scss/style.scss) записано: ' + SCSSfileImport);
+                                    } else {
+                                        console.log('[NTH] ОШИБКА записи в '+ dirs.source + '/scss/style.scss: ' + err);
+                                    }
+                                });
+                            } else {
+                                console.log('[NTH] ОШИБКА открытия '+ dirs.source + '/scss/style.scss: ' + err);
+                            }
+                        });
+                    }
+                    else {
+                        console.log('[NTH] Импорт НЕ прописан в '+ dirs.source + '/scss/style.scss (он там уже есть)');
+                    }
+                }
+
+                // Если это html
+                else if(extention == 'html') {
+                    fileCreateMsg = '[NTH] Для вставки разметки: @@include(\'blocks/' + blockName + '/' + blockName + '.html\') Подробнее: https://www.npmjs.com/package/gulp-file-include';
+                }
+
+                // Если это JS
+                else if(extention == 'js') {
+                    fileContent = '// (function(){\n// код\n// }());\n';
+                }
+
+                // Если нужна подпапка для картинок
+                else if(extention == 'img') {
+                    let imgFolder = dirPath + 'img/';
+                    if(fileExist(imgFolder) === false) {
+                        mkdirp(imgFolder, function (err) {
+                            if (err) console.error(err)
+                            else console.log('[NTH] Папка создана: ' + imgFolder)
+                        });
+                    }
+                    else {
+                        console.log('[NTH] Папка НЕ создана (уже существует) ')
+                    }
+                }
+
+                // Создаем файл, если он еще не существует
+                if(fileExist(filePath) === false && extention !== 'img') {
+                    fs.writeFile(filePath, fileContent, function(err) {
+                        if(err) {
+                            return console.log('[NTH] Файл НЕ создан: ' + err);
+                        }
+                        console.log('[NTH] Файл создан: ' + filePath);
+                        if(fileCreateMsg) {
+                            console.warn(fileCreateMsg);
+                        }
+                    });
+                }
+                else if(extention !== 'img') {
+                    console.log('[NTH] Файл НЕ создан: ' + filePath + ' (уже существует)');
+                }
+            });
+        }
+    });
+}
+else {
+    console.log('[NTH] Отмена операции: не указан блок');
+}
+
+// Оставить в массиве только уникальные значения (убрать повторы)
+function uniqueArray(arr) {
+    var objectTemp = {};
+    for (var i = 0; i < arr.length; i++) {
+        var str = arr[i];
+        objectTemp[str] = true; // запомнить строку в виде свойства объекта
+    }
+    return Object.keys(objectTemp);
+}
+
+// Проверка существования файла
+function fileExist(path) {
+    const fs = require('fs');
+    try {
+        fs.statSync(path);
+    } catch(err) {
+        return !(err && err.code === 'ENOENT');
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,368 @@
+'use strict';
+
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const server = require('browser-sync').create();
+const minify = require('gulp-csso');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const include = require('gulp-file-include');
+const sort = require('postcss-sorting');
+
+gulp.task('sorting', function () {
+    console.log('---------- сортировка SCSS');
+    return gulp.src('source/blocks/**/*.scss')
+        .pipe(plumber({errorHandler: notify.onError()}))
+        .pipe(postcss([
+            sort({
+                "order": [
+                    "custom-properties",
+                    "dollar-variables",
+                    "at-rules",
+                    {
+                        "type": "at-rule",
+                        "name": "include"
+                    },
+                    {
+                        "type": "at-rule",
+                        "name": "include",
+                        "parameter": "icon"
+                    },
+                    "declarations",
+                    "rules"
+                ],
+                "properties-order": [
+                    {
+                        "emptyLineBefore": true,
+                        "properties": [
+                            "content",
+                            "position",
+                            "top",
+                            "right",
+                            "bottom",
+                            "left",
+                            "z-index"
+                        ]
+                    },
+                    {
+                        "emptyLineBefore": true,
+                        "properties": [
+                            "display",
+                            "flex",
+                            "flex-grow",
+                            "flex-shrink",
+                            "flex-basis",
+                            "flex-flow",
+                            "flex-direction",
+                            "flex-wrap",
+                            "justify-content",
+                            "align-content",
+                            "align-items",
+                            "order",
+                            "align-self",
+                            "float",
+                            "clear"
+                        ]
+                    },
+                    {
+                        "emptyLineBefore": true,
+                        "properties": [
+                            "box-sizing",
+                            "width",
+                            "min-width",
+                            "max-width",
+                            "height",
+                            "min-height",
+                            "max-height",
+                            "margin",
+                            "margin-top",
+                            "margin-right",
+                            "margin-bottom",
+                            "margin-left",
+                            "padding",
+                            "padding-top",
+                            "padding-right",
+                            "padding-bottom",
+                            "padding-left",
+                            "overflow",
+                            "overflow-x",
+                            "overflow-y"
+                        ]
+                    },
+                    {
+                        "emptyLineBefore": true,
+                        "properties": [
+                            "list-style",
+                            "list-style-position",
+                            "list-style-type",
+                            "list-style-image",
+                            "border-collapse",
+                            "border-spacing",
+                            "table-layout",
+                            "empty-cells",
+                            "caption-side",
+                            "font",
+                            "font-weight",
+                            "font-size",
+                            "line-height",
+                            "font-family",
+                            "vertical-align",
+                            "text-align",
+                            "direction",
+                            "color",
+                            "text-transform",
+                            "text-decoration",
+                            "font-style",
+                            "font-variant",
+                            "font-size-adjust",
+                            "font-stretch",
+                            "font-effect",
+                            "font-emphasize",
+                            "font-emphasize-position",
+                            "font-emphasize-style",
+                            "font-smooth",
+                            "text-align-last",
+                            "letter-spacing",
+                            "word-spacing",
+                            "white-space",
+                            "text-emphasis",
+                            "text-emphasis-color",
+                            "text-emphasis-style",
+                            "text-emphasis-position",
+                            "text-indent",
+                            "text-justify",
+                            "text-outline",
+                            "text-wrap",
+                            "text-overflow",
+                            "text-overflow-ellipsis",
+                            "text-overflow-mode",
+                            "text-orientation",
+                            "word-wrap",
+                            "word-break",
+                            "tab-size",
+                            "hyphens",
+                            "unicode-bidi",
+                            "columns",
+                            "column-count",
+                            "column-fill",
+                            "column-gap",
+                            "column-rule",
+                            "column-rule-color",
+                            "column-rule-style",
+                            "column-rule-width",
+                            "column-span",
+                            "column-width",
+                            "text-shadow",
+                            "page-break-after",
+                            "page-break-before",
+                            "page-break-inside"
+                        ]
+                    },
+                    {
+                        "emptyLineBefore": true,
+                        "properties": [
+                            "background",
+                            "background-color",
+                            "background-image",
+                            "background-repeat",
+                            "background-position",
+                            "background-position-x",
+                            "background-position-y",
+                            "background-size",
+                            "background-clip",
+                            "background-origin",
+                            "background-attachment",
+                            "box-decoration-break",
+                            "background-blend-mode",
+                            "border",
+                            "border-width",
+                            "border-style",
+                            "border-color",
+                            "border-top",
+                            "border-top-width",
+                            "border-top-style",
+                            "border-top-color",
+                            "border-right",
+                            "border-right-width",
+                            "border-right-style",
+                            "border-right-color",
+                            "border-bottom",
+                            "border-bottom-width",
+                            "border-bottom-style",
+                            "border-bottom-color",
+                            "border-left",
+                            "border-left-width",
+                            "border-left-style",
+                            "border-left-color",
+                            "border-radius",
+                            "border-top-left-radius",
+                            "border-top-right-radius",
+                            "border-bottom-right-radius",
+                            "border-bottom-left-radius",
+                            "border-image",
+                            "border-image-source",
+                            "border-image-slice",
+                            "border-image-width",
+                            "border-image-outset",
+                            "border-image-repeat",
+                            "outline",
+                            "outline-width",
+                            "outline-style",
+                            "outline-color",
+                            "outline-offset",
+                            "box-shadow",
+                            "visibility",
+                            "cursor",
+                            "opacity",
+                            "filter"
+                        ]
+                    },
+                    {
+                        "emptyLineBefore": true,
+                        "properties": [
+                            "transition",
+                            "transition-delay",
+                            "transition-timing-function",
+                            "transition-duration",
+                            "transition-property",
+                            "animation",
+                            "animation-name",
+                            "animation-duration",
+                            "animation-play-state",
+                            "animation-timing-function",
+                            "animation-delay",
+                            "animation-iteration-count",
+                            "animation-direction",
+                            "transform",
+                            "transform-origin",
+                            "backface-visibility",
+                            "perspective",
+                            "perspective-origin",
+                            "transform-style"
+                        ]
+                    },
+                    {
+                        "emptyLineBefore": true,
+                        "properties": [
+                            "quotes",
+                            "counter-reset",
+                            "counter-increment",
+                            "resize",
+                            "user-select",
+                            "nav-index",
+                            "nav-up",
+                            "nav-right",
+                            "nav-down",
+                            "nav-left",
+                            "pointer-events",
+                            "will-change",
+                            "clip",
+                            "clip-path",
+                            "zoom"
+                        ]
+                    }
+                ],
+                "unspecified-properties-position": "bottom"
+            })
+        ]))
+        .pipe(gulp.dest('source/blocks'));
+});
+
+gulp.task('html', function () {
+    console.log('---------- сборка HTML');
+    return gulp.src('source/pages/*.html')
+        .pipe(plumber({errorHandler: notify.onError()}))
+        .pipe(include({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function () {
+    console.log('---------- очистка папки сборки');
+    return del('build');
+});
+
+gulp.task('copy', function () {
+    console.log('---------- копирование файлов в папку сборки');
+    return gulp.src([
+        'source/fonts/**/*.{woff,woff2}',
+        'source/img/**',
+        'source/js/**'
+    ], {
+        base: 'source'
+    })
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('style', function () {
+    console.log('---------- сборка CSS');
+    return gulp.src('source/scss/style.scss')
+        .pipe(plumber({errorHandler: notify.onError()}))
+        .pipe(scss())
+        .pipe(postcss([
+            autoprefixer({
+                browsers: [
+                    'last 1 version',
+                    'last 2 Chrome versions',
+                    'last 2 Firefox versions',
+                    'last 2 Opera versions',
+                    'last 2 Edge versions'
+                ]
+            }),
+            mqpacker({
+                sort: true
+            })
+        ]))
+        .pipe(gulp.dest('build/css'))
+        .pipe(minify())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('images', function () {
+    console.log('---------- оптимизация изображений');
+    return gulp.src('build/img/**/*.{png,img,gif}')
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true})
+        ]))
+        .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('watch', function () {
+    console.log('---------- запуск слежения за изменениями');
+    gulp.watch('source/scss/*.scss', gulp.series('style'));
+    gulp.watch('source/blocks/**/*.scss', gulp.series('style'));
+    gulp.watch('source/pages/*.html', gulp.series('html'));
+    gulp.watch('source/blocks/**/*.html', gulp.series('html'));
+});
+
+gulp.task('serve', function () {
+    console.log('---------- запуск сервера');
+    server.init({
+        server: 'build',
+        index: 'index.html'
+    });
+    server.watch('build/css/*.css').on('change', server.reload);
+    server.watch('build/*.html').on('change', server.reload);
+});
+
+gulp.task('default',
+    gulp.series('clean', 'copy', 'html', 'style', 'images',
+        gulp.parallel('watch', 'serve')));
+
+// Отправка в GH pages (ветку gh-pages репозитория)
+gulp.task('deploy', function() {
+    const ghPages = require('gulp-gh-pages');
+    console.log('---------- Публикация содержимого ./build/ на GH pages');
+    return gulp.src('./build' + '**/*')
+        .pipe(ghPages());
+});
